a99890952c4cde725389f90d8e3dc8b6abffb618
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2019-06-19 23:13:41 +0200

    removed dist files

--- dist/libs/debug-render-time.js
+++
@@ -1,9 +0,0 @@
-(function () {
-	'use strict';
-
-	let started;
-	nxtx.on('prerender', () => started = Date.now());
-	nxtx.on('postrender', () => console.log(`rendering document took ${Date.now() - started}ms`));
-
-}());
-//# sourceMappingURL=debug-render-time.js.map
