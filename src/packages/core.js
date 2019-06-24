10ba60328ba334006d3a4402a07b2ddcf32d71e1
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2019-06-24 02:21:10 +0200

    Create core.js file for base package library

---
+++ src/packages/core.js
@@ -0,0 +1,12 @@
+import formatting from './basic-formatting';
+import layout from './layout';
+import loading from './loading';
+import styling from './styling';
+
+
+export default {
+    formatting,
+    layout,
+    loading,
+    styling
+}
