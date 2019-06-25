a298205ebe3f694ab97d132ff780cd3ad1a3ea42
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2019-06-26 00:49:41 +0200

    Make bibliography package exportable

--- src/packages/bibliography.js
+++ src/packages/bibliography.js
@@ -11,26 +11,11 @@ const style = document.createElement("style");
 style.id = 'bibliography-style-block';
 document.head.appendChild(style);
 
+// Default
+style.sheet.insertRule('.bib-entry { margin-bottom: 5px }', 0);
+style.sheet.insertRule(`.bib-entry span { margin-right: 5px }`, 1);
 
-nxtx.registerCommand('bib-add', (key, entry) => {
-    if (!entry || entry.type !== 'dictionary') return console.error('bib-add only must have a dictionary as second argument');
-    entries[key.value] = entry.value;
-});
 
-nxtx.registerCommand('cite', async (...args) => {
-    const cites = args.flatMap(arg => {
-        let index = cited.indexOf(arg.value);
-        if (index === -1) {
-            cited.push(arg.value);
-            index = cited.length - 1;
-        }
-        const e = nxtx.htmlLite('a', { href: `#---${arg.value}`});
-        e.innerText = index + 1;
-        return [ e, ', ' ];
-    });
-    cites.length -= 1;
-    return nxtx.html('span', null, '[', ...cites, ']');
-});
 
 const formatAuthors = author => {
     const authors = author.value.split(' and ').map(a => {
@@ -51,26 +36,55 @@ const entryFieldFormatting = {
     url: url => nxtx.html('a', { class: 'bib bib-url', href: url.value, target: '_blank' }, url.value),
     isbn: isbn => nxtx.html('span', { class: 'bib bib-isbn' }, `ISBN: ${isbn.value}.`)
 };
-nxtx.registerCommand('bib-print', async () => {
-    const citedEntries = cited.map(e => entries[e] || console.warn(`Bibliography entry '${e}' not found`)).filter(f => !!f);
-    if (citedEntries.length === 0) {
-        return;
-    }
-    const mapped = citedEntries.map((entryFields, number) => nxtx.html('div', { id: `---${cited[number]}`, class: 'bib bib-entry'},
-            nxtx.html('span', { class: 'bib bib-ordering'}, `[${number + 1}]`),
-            ...Object.keys(entryFieldFormatting)
-                .filter(k => entryFields[k] !== undefined)
-                .map(k => entryFieldFormatting[k](entryFields[k]))
-    ));
-    return [
-        { type: 'command', name: 'pagebreak', args: [] },
-        nxtx.html('h2', { class: 'bibliography' }, 'Bibliography'),
-        ...mapped
-    ];
-});
 
 nxtx.on('prerender', () => cited = []);
 
-// Default
-style.sheet.insertRule('.bib-entry { margin-bottom: 5px }', 0);
-style.sheet.insertRule(`.bib-entry span { margin-right: 5px }`, 1);
+const pkg = {
+    name: 'basic-formatting',
+    commands: {
+        'cite': async (...args) => {
+            const cites = args.flatMap(arg => {
+                let index = cited.indexOf(arg.value);
+                if (index === -1) {
+                    cited.push(arg.value);
+                    index = cited.length - 1;
+                }
+                const e = nxtx.htmlLite('a', { href: `#---${arg.value}`});
+                e.innerText = index + 1;
+                return [ e, ', ' ];
+            });
+            cites.length -= 1;
+            return nxtx.html('span', null, '[', ...cites, ']');
+        },
+        'bib-print': async () => {
+            const citedEntries = cited.map(e => entries[e] || console.warn(`Bibliography entry '${e}' not found`)).filter(f => !!f);
+            if (citedEntries.length === 0) {
+                return;
+            }
+            const mapped = citedEntries.map((entryFields, number) => nxtx.html('div', { id: `---${cited[number]}`, class: 'bib bib-entry'},
+                nxtx.html('span', { class: 'bib bib-ordering'}, `[${number + 1}]`),
+                ...Object.keys(entryFieldFormatting)
+                    .filter(k => entryFields[k] !== undefined)
+                    .map(k => entryFieldFormatting[k](entryFields[k]))
+            ));
+            return [
+                { type: nxtx.TYPE.COMMAND, name: 'pagebreak', args: [] },
+                nxtx.html('h2', {class: 'bibliography'}, 'Bibliography'),
+                ...mapped
+            ];
+        }
+    },
+    preprocessors: {
+        'bib-add': (key, entry) => {
+            if (!entry || entry.type !== nxtx.TYPE.DICTIONARY) return console.error('bib-add only must have a dictionary as second argument');
+            entries[key.value] = entry.value;
+        },
+    }
+};
+
+if (nxtx) {
+    Object.keys(pkg.commands).forEach(name => nxtx.registerCommand(name, pkg.commands[name]));
+    Object.keys(pkg.preprocessors).forEach(name => nxtx.registerPreprocessor(name, pkg.preprocessors[name]));
+}
+
+export default pkg;
