677595788cb03caaa85e55517c21eed10848d9c2
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2019-06-26 00:51:59 +0200

    Update demo

--- docs/demo/packages/debug-render-time.js
+++ docs/demo/packages/debug-render-time.js
@@ -1,13 +1,2 @@
-(function () {
-    'use strict';
-
-    /*  Package printing rendering time for diagnostics
-        Author: Malte Rosenbjerg
-        License: MIT */
-
-    let started;
-    nxtx.on('prerender', () => started = Date.now());
-    nxtx.on('postrender', () => console.log(`rendering document took ${Date.now() - started}ms`));
-
-}());
+!function(){"use strict";let n;nxtx.on("prerender",()=>n=Date.now()),nxtx.on("postrender",()=>console.log(`rendering document took ${Date.now()-n}ms`))}();
 //# sourceMappingURL=debug-render-time.js.map
