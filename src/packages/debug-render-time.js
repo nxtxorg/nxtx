1b5dcb41db64e8a781b2065a4e3455f7cba631b2
Malte Rosenbjerg <maro@netcompany.com>
2019-07-20 19:25:44 +0200

    Remove old js versions

--- src/packages/debug-render-time.js
+++
@@ -1,7 +0,0 @@
-/*  Package printing rendering time for diagnostics
-    Author: Malte Rosenbjerg
-    License: MIT */
-
-let started;
-nxtx.on('prerender', () => started = Date.now());
-nxtx.on('postrender', () => console.log(`rendering document took ${Date.now() - started}ms`));
